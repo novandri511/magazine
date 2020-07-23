0d86b606a3e78c1f8b67a722887bfd7c279aff86
novandri511 <45961623+novandri511@users.noreply.github.com>
2020-07-23 14:08:49 +0700

    push 2

---
+++ assets/script/main.js
@@ -0,0 +1,17 @@
+$('.owl-carousel').owlCarousel({
+    stagePadding: 50,
+    loop: true,
+    margin: 50,
+
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 1
+        },
+        1000: {
+            items: 1
+        }
+    }
+})
